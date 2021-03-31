e0bad9810faee3179ad8076b888b15a9fbac99be
Kevin Akkoyun <kevin.akkoyun@outlook.com>
2021-03-31 12:17:31 +0300

    Started soundboard v2.0 rework

--- src/main/java/otp/group6/controller/SoundboardController.java
+++ src/main/java/otp/group6/controller/SoundboardController.java
@@ -1,7 +1,5 @@
 package otp.group6.controller;
 
-import javax.sound.sampled.LineListener;
-
 import otp.group6.AudioEditor.Soundboard;
 
 /**
@@ -31,79 +29,5 @@ public class SoundboardController {
 			instance = new SoundboardController();
 		}
 		return instance;
-	}
-
-	public void playSound(int index) {
-		if (soundboard.checkSampleArray(index)) {
-			soundboard.playSample(index);
-		} else {
-			System.out.println("index not found - sample array: " + index);
-		}
-
-	}
-
-	public void stopSound() {
-		soundboard.stopSample();
-	}
-
-	public boolean isPlaying() {
-		return soundboard.isPlaying();
-	}
-
-	/**
-	 * Adds sample to Soundboard SampleArray
-	 * 
-	 * @param path filepath
-	 */
-	public void addSample(String path) {
-		try {
-			soundboard.addSample(path);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public void editSample(String path, int index) {
-		soundboard.editSample(path, index);
-	}
-
-	public void removeSample(int index) {
-		if (soundboard.checkSampleArray(index)) {
-			soundboard.removeSample(index);
-		} else {
-			System.out.println("index not found - sample array: " + index);
-		}
-	}
-
-	public String getSampleName(int index) {
-		return soundboard.getSampleName(index);
-	}
-
-	public void setSampleName(int index, String name) {
-		soundboard.setSampleName(index, name);
-	}
-
-	public int getSampleArrayLength() {
-		return soundboard.getSampleArrayLength();
-	}
-
-	public void saveSampleData() {
-		soundboard.saveSampleData();
-	}
-
-	public void readSampleData() {
-		soundboard.readSampleData();
-	}
-
-	public void clearSampleData() {
-		soundboard.clearSampleData();
-	}
-
-	public void setListener(LineListener listener) {
-		soundboard.setListener(listener);
-	}
-
-	public void removeListener(LineListener listener) {
-		soundboard.removeListener(listener);
 	}
 }
