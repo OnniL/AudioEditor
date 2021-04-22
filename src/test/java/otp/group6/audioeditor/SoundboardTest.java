6dbb3c0c48d3e7a59932b9f60ac3015bb6d1eafc
Kevin Akkoyun <kevin.akkoyun@outlook.com>
2021-04-22 16:08:56 +0300

    fixing tests + added saveData test

--- src/test/java/otp/group6/audioeditor/SoundboardTest.java
+++ src/test/java/otp/group6/audioeditor/SoundboardTest.java
@@ -28,7 +28,7 @@ class SoundboardTest {
 	@Test
 	final void testCreateSampleFPath() {
 
-		int testSampleIndex = soundboard.addSample("src/audio/jumalauta.wav");
+		int testSampleIndex = soundboard.addSample("src/audio/testiaani.wav");
 
 		assertTrue(soundboard.checkSampleArray(testSampleIndex), "Checks if sample array contains added sample");
 	}
@@ -38,7 +38,7 @@ class SoundboardTest {
 	@Test
 	final void testCreateSampleNew() {
 
-		Sample testSample = soundboard.new Sample("src/audio/jumalauta.wav");
+		Sample testSample = soundboard.new Sample("src/audio/testiaani.wav");
 
 		int testSampleIndex = soundboard.addSample(testSample);
 
@@ -50,7 +50,7 @@ class SoundboardTest {
 	@Test
 	final void testSampleRemovalWithIndex() {
 
-		Sample newSample = soundboard.new Sample("src/audio/jumalauta.wav");
+		Sample newSample = soundboard.new Sample("src/audio/testiaani.wav");
 		int testSampleIndex = soundboard.addSample(newSample);
 
 		Sample removedSample = soundboard.removeSample(testSampleIndex);
@@ -63,7 +63,7 @@ class SoundboardTest {
 	@Test
 	final void testSampleRemovalWithSample() {
 
-		Sample newSample = soundboard.new Sample("src/audio/jumalauta.wav");
+		Sample newSample = soundboard.new Sample("src/audio/testiaani.wav");
 		soundboard.addSample(newSample);
 
 		assertTrue(soundboard.removeSample(newSample), "Removing sample");
@@ -72,10 +72,10 @@ class SoundboardTest {
 	@DisplayName("Save sample data and read it")
 	@Order(5)
 	@Test
-	@Disabled
+
 	final void testSaveSampleData() {
 		for (int i = 0; i < 10; i++) {
-			soundboard.addSample("src/audio/jumalauta.wav");
+			soundboard.addSample("src/audio/testiaani.wav");
 		}
 		assertEquals(10, soundboard.getSampleArrayLength(), "sampleArray didnt recieve all samples");
 		soundboard.saveSampleData();
