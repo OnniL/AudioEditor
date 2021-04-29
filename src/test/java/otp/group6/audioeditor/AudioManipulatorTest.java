7f5e574236f95f00f67787f7beb53441eb788489
OnniL <onni.lukkarila@gmail.com>
2021-04-29 21:41:20 +0300

    Manipulator test fixes deleted wrong import

--- src/test/java/otp/group6/audioeditor/AudioManipulatorTest.java
+++ src/test/java/otp/group6/audioeditor/AudioManipulatorTest.java
@@ -4,9 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.File;
 
-import org.junit.BeforeClass;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
